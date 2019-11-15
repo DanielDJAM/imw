74aabd95fdc6a013990e10a3177ed3f8e31368a6
alu5894 <dany_am95@hotmail.com>
2019-11-15 10:39:50 +0000

    a4

--- ut2/a4/program3.py
+++ ut2/a4/program3.py
@@ -1,13 +1,14 @@
 import sys
 
-num = int(sys.argv[1])
-Chain_String = str(sys.argv[2])
-
-if num < 0:
+size_in = int(sys.argv[1])
+chain_string = sys.argv[2]
+count = 0
+if size_in < 0:
     print('Error.Saliendo del programa...')
 else:
-    for i in Chain_String:
-        Chain_List = Chain_String.split()
-        size = len(Chain_List)
-        result = Chain_List[num]
-        print(result)
+    chain_list = chain_string.split()
+    for word in chain_list:
+        size_out = len(word)
+        if size_in == size_out:
+            count += 1
+    print(f'Hay {count} palabras con {size_in} caracteres')
