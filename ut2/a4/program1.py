017b1ec935a4167a72faec4c02fe0df42cb4e462
alu5894 <dany_am95@hotmail.com>
2019-11-21 10:36:33 +0000

    a4_final

--- ut2/a4/program1.py
+++ ut2/a4/program1.py
@@ -4,8 +4,8 @@ num = int(sys.argv[1])
 
 if len(str(num)) == 8:
     value1 = num % 23
-    DNI = 'TRWAGMYFPDXBNJZSQVHLCKE'
-    print(f'{num}{DNI[value1]}')
-    
+    dni = 'TRWAGMYFPDXBNJZSQVHLCKE'
+    print(f'{num}{dni[value1]}')
+
 else:
     print('Introduzca 8 dígitos')
