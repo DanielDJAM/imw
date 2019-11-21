856f2aaf7300b0a252aa9d53417e34d54d13610a
alu5894 <dany_am95@hotmail.com>
2019-11-21 12:07:22 +0000

    a4_final

---
+++ ut2/a4/program5.py
@@ -0,0 +1,17 @@
+import sys
+
+chain_num = sys.argv[1:]
+count_num = 0
+count_chain = 0
+range = 0
+
+for value1 in chain_num:
+    range = value1 - range
+    chain = 
+
+
+
+
+    count_chain = float(value1) + count_chain
+final = count_chain / count_num
+print(f'La media de los valores es:{final}')
