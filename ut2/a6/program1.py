415e9fe5d2750f0be72d7e044caae13d9a3df1ec
alu5894 <dany_am95@hotmail.com>
2019-11-28 09:58:34 +0000

    a6

---
+++ ut2/a6/program1.py
@@ -0,0 +1,16 @@
+import sys
+
+text = str(sys.argv[1: ])
+
+def count_words(text):
+    summary = {}
+    text2 = text.split()
+    for words in text2:
+        if words in summary:
+            summary[words] += 1
+        else:
+            summary[words] += 1
+    return summary
+
+if __name__ == '__main__':
+    print(f'''lista:{count_words(text)}''')
