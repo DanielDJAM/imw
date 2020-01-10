a4281af029282c3d5b4aa97320f9e6fba2c07c7b
alu5894 <dany_am95@hotmail.com>
2020-01-10 10:31:26 +0000

    a1

--- ut3/a1/fabfile.py
+++ ut3/a1/fabfile.py
@@ -1,11 +1,11 @@
 from fabric.api import env, cd, local, run
 
 # nombre de la máquina de producción
-env.hosts = ['vmweb.alu5894.es']
+env.hosts = ['vmweb.alu5894.me']
 
 
 def deploy():
     local('git push')
-    with cd('~/webapps/myweb'):
+    with cd('~/webapps/vmweb'):
       run('git pull')
-    run('supervisorctl restart myweb')
+    run('supervisorctl restart vmweb')
