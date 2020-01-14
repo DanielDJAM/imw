677cd652ae4cc95e368cf98c6ff1e2d8c906e552
DJAM <dany_am95@hotmail.com>
2020-01-14 12:45:59 +0000

    ut3-a1-final

--- ut3/a1/mysql.py
+++ ut3/a1/mysql.py
@@ -1,19 +1,29 @@
 import pymysql.cursors
 
+
 class DB():
-    def __init__(self, username, password, database):
+    def _make_connection(self):
         self.connection = pymysql.connect(
-                host='localhost',
-                port=3306,
-                user='emmet',
-                password='brown',
-                db='vmweb',
-                charset='utf8mb4',
-                cursorclass=pymysql.cursors.DictCursor,
-                autocommit=True
-                )
+            host='localhost',
+            port=3306,
+            user=self.username,
+            password=self.password,
+            db=self.database,
+            charset='utf8mb4',
+            cursorclass=pymysql.cursors.DictCursor,
+            autocommit=True
+        )
+
+    def __init__(self, username, password, database):
+        self.username = username
+        self.password = password
+        self.database = database
+        self._make_connection()
 
     def run(self, sql):
+        if not self.connection.open:
+            self._make_connection()
+
         with self.connection.cursor() as cursor:
             cursor.execute(sql)
             if sql.startswith('select'):
